789463079305910a3db056a2a1ad4a2be29f1c96
Shivani Goel <shivanig@andrew.cmu.edu>
2017-12-10 17:36:30 -0500

    Problem 20 for Final Project

---
+++ hello_aliens.R
@@ -0,0 +1,7 @@
+cat("Hello! It is great to meet you.", 
+"We come from a planet called Earth that is similar to your planet.",
+"We do not mean you any harm. We would like to know more about your planet and your people.",
+"How many of you are there?",
+"Do you know of other planets like yours out there?",
+"Have you traveled into space?",
+"We should sit and talk, to get to know our different races.")
